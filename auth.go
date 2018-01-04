9cc4db92d6f58c7836dbcec55168aa0aa2444dc2
Yuma Nemoto <>
2018-01-04 23:29:23 +0900

    Add Auth Providers

--- auth.go
+++ auth.go
@@ -1,36 +1,38 @@
 package main
 
 import (
-	"github.com/stretchr/gomniauth"
-	"net/http"
-	"log"
 	"fmt"
+	"net/http"
 	"strings"
+
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/objx"
 )
 
 type authHandler struct {
-	next	http.Handler	
+	next http.Handler
 }
 
 func (h *authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if _, err := r.Cookie("auth"); err == http.ErrNoCookie {
+	_, err := r.Cookie("auth")
+	if err == http.ErrNoCookie {
 		// not authenticated
 		w.Header().Set("Location", "/login")
 		w.WriteHeader(http.StatusTemporaryRedirect)
-	} else if err != nil {
-		// some other error
-		panic(err.Error())
-	} else {
-		// success - call the next handler
-		h.next.ServeHTTP(w, r)
+		return
 	}
+	if err != nil {
+		// some other error
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	// success - call the next handler
+	h.next.ServeHTTP(w, r)
 }
 
 // MustAuth adapts handler to ensure authentication has occurred.
 func MustAuth(handler http.Handler) http.Handler {
-	return &authHandler{
-		next:	handler,
-	}
+	return &authHandler{next: handler}
 }
 
 // loginHandler handles the third-party login process.
@@ -40,18 +42,58 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 	provider := segs[3]
 	switch action {
 	case "login":
+
 		provider, err := gomniauth.Provider(provider)
 		if err != nil {
-			log.Fatalln("failed to get auth provider:", provider, "-", err)
+			http.Error(w, fmt.Sprintf("Error when trying to get provider %s: %s", provider, err), http.StatusBadRequest)
+			return
 		}
-		loginUrl, err := provider.GetBeginAuthURL(nil, nil)
+
+		loginURL, err := provider.GetBeginAuthURL(nil, nil)
 		if err != nil {
-			log.Fatalln("failed to call GetBeginAuthURL:", provider, "-", err)
+			http.Error(w, fmt.Sprintf("Error when trying to GetBeginAuthURL for %s: %s", provider, err), http.StatusInternalServerError)
+			return
 		}
-		w.Header().Set("Location", loginUrl)
+
+		w.Header().Set("Location", loginURL)
 		w.WriteHeader(http.StatusTemporaryRedirect)
-	default: 
+
+	case "callback":
+
+		provider, err := gomniauth.Provider(provider)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Error when trying to get provider %s: %s", provider, err), http.StatusBadRequest)
+			return
+		}
+
+		// get the credentials
+		creds, err := provider.CompleteAuth(objx.MustFromURLQuery(r.URL.RawQuery))
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Error when trying to complete auth for %s: %s", provider, err), http.StatusInternalServerError)
+			return
+		}
+
+		// get the user
+		user, err := provider.GetUser(creds)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Error when trying to get user from %s: %s", provider, err), http.StatusInternalServerError)
+			return
+		}
+
+		// save some data
+		authCookieValue := objx.New(map[string]interface{}{
+			"name": user.Name(),
+		}).MustBase64()
+		http.SetCookie(w, &http.Cookie{
+			Name:  "auth",
+			Value: authCookieValue,
+			Path:  "/"})
+
+		w.Header().Set("Location", "/chat")
+		w.WriteHeader(http.StatusTemporaryRedirect)
+
+	default:
 		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprintf(w, "アクション%sには非対応です", action)
+		fmt.Fprintf(w, "Auth action %s not supported", action)
 	}
 }
--- main.go
+++ main.go
@@ -1,56 +1,72 @@
 package main
 
 import (
-  "log"
-  "net/http"
-  "text/template"
-  "path/filepath"
-  "sync"
-  "flag"
+	"flag"
+	"log"
+	"net/http"
+	"path/filepath"
+	"sync"
+	"text/template"
 
-	"github.com/stretchr/gomniauth"  
+	"github.com/stretchr/gomniauth"
 	"github.com/stretchr/gomniauth/providers/facebook"
 	"github.com/stretchr/gomniauth/providers/github"
 	"github.com/stretchr/gomniauth/providers/google"
+	"github.com/stretchr/objx"
 )
 
+// templ represents a single template
 type templateHandler struct {
-  once      sync.Once
-  filename  string
-  templ     *template.Template
+	once     sync.Once
+	filename string
+	templ    *template.Template
 }
 
+// ServeHTTP handles the HTTP request.
 func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-  t.once.Do(func(){
-    t.templ =
-      template.Must(template.ParseFiles(filepath.Join("templates",
-        t.filename)))
-  })
-  t.templ.Execute(w, r)
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+
+	data := map[string]interface{}{
+		"Host": r.Host,
+	}
+	if authCookie, err := r.Cookie("auth"); err == nil {
+		data["UserData"] = objx.MustFromBase64(authCookie.Value)
+	}
+
+	t.templ.Execute(w, data)
 }
+
+var host = flag.String("host", ":8080", "The host of the application.")
 
 func main() {
-  var addr = flag.String("addr", ":8080", "Application Address")
-  flag.Parse()
 
-  // setup gomniauth
-  gomniauth.SetSecurityKey("AIzaSyAJaYYoorVvGKJqQ0wUVOykfCvwHhDhwq4")
-  gomniauth.WithProviders(
-    facebook.New("208178147561-16rib2nc7hsbgjnde8jtb9k2gm5v5433", "LriuzY2vOdY_HK-UIfPoEHPc", "http://localhost:8080/auth/callback/facebook"),
-    github.New("208178147561-65vab8kbmsqbbpa1m5c26g9bpbijjbt4", "RPne_8U_UzUKymZJojSaJIDY", "http://localhost:8080/auth/callback/github"),
-    google.New("208178147561-jp8v2fdnf3scet47rhudi6dk2963ebjc.apps.googleusercontent.com", "zc4MlTeQ8qwuCr34OAn2gjIC", "http://localhost:8080/auth/callback/google"),
-  )
-  r := newRoom()
-  // route
-  http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
-  http.Handle("/login", &templateHandler{filename: "login.html"})
-  http.HandleFunc("/auth", loginHandler)
-  http.Handle("/room", r)
-  // start chat room
-  go r.run()
-  // start web server
-  log.Println("Start Web Server Port: ", *addr)
-  if err := http.ListenAndServe(":8080", nil); err != nil {
-    log.Fatal("ListenAndServe: ", err)
-  }
+	flag.Parse() // parse the flags
+
+	// setup gomniauth
+	gomniauth.SetSecurityKey("98dfbg7iu2nb4uywevihjw4tuiyub34noilk")
+	gomniauth.WithProviders(
+		github.New("65vab8kbmsqbbpa1m5c26g9bpbijjbt4", "RPne_8U_UzUKymZJojSaJIDY", "http://localhost:8080/auth/callback/github"),
+		google.New("208178147561-jp8v2fdnf3scet47rhudi6dk2963ebjc.apps.googleusercontent.com", "zc4MlTeQ8qwuCr34OAn2gjIC", "http://localhost:8080/auth/callback/google"),
+		facebook.New("16rib2nc7hsbgjnde8jtb9k2gm5v5433", "LriuzY2vOdY_HK-UIfPoEHPc", "http://localhost:8080/auth/callback/facebook"),
+	)
+
+	r := newRoom()
+	// r.tracer = trace.New(os.Stdout)
+
+	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
+	http.Handle("/login", &templateHandler{filename: "login.html"})
+	http.HandleFunc("/auth/", loginHandler)
+	http.Handle("/room", r)
+
+	// get the room going
+	go r.run()
+
+	// start the web server
+	log.Println("Starting web server on", *host)
+	if err := http.ListenAndServe(*host, nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+
 }
