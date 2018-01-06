40bc010588a7b7aa6e7d2087029c05a827394709
Yuma Nemoto <nemoto1257@gmail.com>
2018-01-06 15:12:05 +0900

    Get Avatar Url

--- auth.go
+++ auth.go
@@ -83,6 +83,7 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 		// save some data
 		authCookieValue := objx.New(map[string]interface{}{
 			"name": user.Name(),
+			"avatar_url": user.AvatarURL(),
 		}).MustBase64()
 		http.SetCookie(w, &http.Cookie{
 			Name:  "auth",
