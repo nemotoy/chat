a38da6087bf461f8e4d192b23bc14303667bc733
Yuma Nemoto <nemoto1257@gmail.com>
2018-01-06 10:46:48 +0900

    Modify code

--- client.go
+++ client.go
@@ -1,31 +1,47 @@
 package main
 
 import (
-  "github.com/gorilla/websocket"
+	"time"
+
+	"github.com/gorilla/websocket"
 )
 
+// client represents a single chatting user.
 type client struct {
-  socket  *websocket.Conn
-  send    chan[]byte
-  room    *room
+
+	// socket is the web socket for this client.
+	socket *websocket.Conn
+
+	// send is a channel on which messages are sent.
+	send chan *message
+
+	// room is the room this client is chatting in.
+	room *room
+
+	// userData holds information about the user
+	userData map[string]interface{}
 }
 
 func (c *client) read() {
-  for {
-    if _, msg, err := c.socket.ReadMessage(); err == nil {
-      c.room.forward <- msg
-    } else {
-      break
-    }
-  }
-  c.socket.Close()
+	defer c.socket.Close()
+	for {
+		var msg *message
+		err := c.socket.ReadJSON(&msg)
+		if err != nil {
+			return
+		}
+		msg.When = time.Now()
+		msg.Name = c.userData["name"].(string)
+		c.room.forward <- msg
+	}
 }
+
 func (c *client) write() {
-  for msg := range c.send {
-    if err := c.socket.WriteMessage(websocket.TextMessage, msg);
-      err != nil {
-        break
-    }
-  }
-  c.socket.Close()
+	defer c.socket.Close()
+	for msg := range c.send {
+		err := c.socket.WriteJSON(msg)
+		if err != nil {
+			return
+		}
+	}
 }
